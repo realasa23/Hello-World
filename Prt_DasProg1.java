3ee15b211c3b1e825bc94f3c46f32caea3e17283
realasa23 <realasa23@gmail.com>
2024-03-07 17:33:30 +0700

    hai guys

---
+++ Prt_DasProg1.java
@@ -0,0 +1,16 @@
+
+package prt_dasprog1;
+public class Prt_DasProg1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        System.out.println("    /\\_____/\\");
+        System.out.println("   /   o   o   \\");
+        System.out.println("  (  ==  ^  ==  ) ");
+        System.out.println("   )           ( ");
+        System.out.println("  (             )");
+        System.out.println("  ( (  )   (  ) )");
+        System.out.println(" (__(__)___(__)__)");
+    }
+    
+}
